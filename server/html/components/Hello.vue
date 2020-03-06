7e186da4670e32f9d11ebc7b83c6d66c22faf953
LEONARD Jerome <jerome-externe.leonard@enedis.fr>
2020-03-06 13:46:15 +0100

    composant

--- server/html/components/Hello.vue
+++ server/html/components/Hello.vue
@@ -1,13 +1,27 @@
 <template>
-    <p>{{ texte }} compagnie! </p>
+    <div>
+        <p>{{ message }} compagnie! </p>
+        <p>{{ reponse }}</p>
+    </div>
 </template>
 
 <script>
-    module.exports = {
-        data: function() {
-            return {
-                texte:'salut'
-            }
-        }   
+  module.exports = {
+    data () {
+      return {
+        message: 'bonjour !',
+        reponse: ''
+      }
+    },
+    created () {
+        fetch("api/session", {
+        "method": "GET"
+        })
+        .then(response => response.json())
+        .then(response => {
+            reponse = response
+            console.log(reponse)
+        });
     }
+  }
 </script>
